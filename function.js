35c7a24f9132b5d221da392f3236b3d7f6337e8d
Gerasimov Ruslan <gerassimov.ruslan@gmail.coom>
2020-03-11 11:17:24 +0300

    function modified to sum odd numbers

--- function.js
+++ function.js
@@ -1,63 +1,57 @@
-function checkIfEven (a, cb) {
+function checkIfEven(a, cb) {
     Homework.mod(a, 2, (result) => {
-        Homework.equal(result, 0, (isEven) => { cb(isEven) });
+        Homework.equal(result, 0, (isEven) => {
+            cb(isEven)
+        });
     });
 }
 
-function sumEven (a, b, cb) {
-    let aIsEven = false;
+function addOddEvenToResult(result, b, isEvenMode, cb) {
     let bIsEven = false;
-    checkIfEven(a, (isEven) => {
-        aIsEven = isEven;
-        checkIfEven(b, (isEven) => {
-            bIsEven = isEven ;
+    checkIfEven(b, (isEven) => {
+        bIsEven = isEven;
+        const bNeedToAdd = isEvenMode ? bIsEven : !bIsEven;
 
-            if(aIsEven && bIsEven) {
-                Homework.add(a, b, cb);
-            } else if(!aIsEven && !bIsEven) {
-                cb(0);
-            } else if(!aIsEven) {
-                cb(b)
-            } else {
-                cb(a)
-            }
-        });
-    } );
+        if (bNeedToAdd) {
+            Homework.add(result, b, cb);
+        } else {
+            cb(result);
+        }
+    });
 }
 
 
-function recursiveSum(arData, result = 0, cb) {
+function recursiveSum(arData, result, $isEven, cb) {
     arData.pop((item) => {
         const itemType = typeof item;
         //Обработка завершения массива (массив законичлся если вернулся undefined и длина массива - 0)
-        if(!item) {
+        if (!item) {
             Homework.equal(itemType, "undefined", () => {
                 arData.length((arLength) => {
-                    if(!arLength) {
+                    if (!arLength) {
                         cb(result);
                     } else {
-                        recursiveSum(arData, result, cb);
+                        recursiveSum(arData, result, $isEven, cb);
                     }
                 })
             });
         } else {
-            if(typeof item === "number"){}
             Homework.equal(itemType, "number", (itemIsNumber) => {
-                if(itemIsNumber) {
-                    sumEven(item, result, (result) => {
-                        recursiveSum(arData, result,  cb);
+                if (itemIsNumber) {
+                    addOddEvenToResult(result, item, $isEven,(result) => {
+                        recursiveSum(arData, result, $isEven, cb);
                     });
                 } else {
-                    recursiveSum(arData, result,  cb);
+                    recursiveSum(arData, result, $isEven, cb);
                 }
             });
         }
     });
 }
 
-function getSumOfEvenNumbersInArray(arData, cb) {
-    if(!(arData instanceof Homework.AsyncArray)) {
+function getSumOddNumbersInArray(arData, cb) {
+    if (!(arData instanceof Homework.AsyncArray)) {
         arData = new Homework.AsyncArray(arData);
     }
-    recursiveSum(arData, 0, cb);
+    recursiveSum(arData, 0, false, cb);
 }
